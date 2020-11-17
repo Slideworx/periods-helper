6d7b02f9ea5b7e69b1287a3d193b8e799c79baa9
Jan-Kanty Pawelski <jan-kanty.pawelski@mtab.com>
2020-11-17 18:02:33 +0100

    Added code

---
+++ src/index.js
@@ -0,0 +1,489 @@
+/**
+ * @typedef {Object} Period
+ *
+ * @property {Object} date
+ *   @property {Date} date.from
+ *   @property {Date} date.to
+ * @property {string} type
+ * @property {string} value
+ */
+
+
+
+const Y = 'Y';
+
+const H = 'H';
+const H_RY = 'HRY';
+const H_YTD = 'HYTD';
+
+const Q = 'Q';
+const Q_RY = 'QRY';
+const Q_YTD = 'QYTD';
+
+const BM = 'BM';
+const BM_RY = 'BMRY';
+const BM_YTD = 'BMYTD';
+
+const M = 'M';
+const M_RY = 'MRY';
+const M_YTD = 'MYTD';
+
+const W = 'W';
+const W_YTD = 'WYTD';
+
+const RY = 'RY';
+const YTD = 'YTD';
+
+
+
+function addLeadingZero(number) {
+  return number < 10 ? `0${ number }` : number;
+}
+
+
+
+/**
+ * @const {Object} dictionary
+ * @access public
+ */
+export const dictionary = {
+  [Y]: {
+    description: 'Years',
+    label: 'Year'
+  },
+
+  [H]: {
+    description: 'Half-years',
+    label: 'Half-year'
+  },
+  [H_RY]: {
+    description: `2 half-years ending at the given ${ H }`,
+    label: 'Rolling year (from half-years)'
+  },
+  [H_YTD]: {
+    description: 'Cumulative half-years from beginning of the year',
+    label: 'Year-to-date (from half-years)'
+  },
+
+  [Q]: {
+    description: 'Quarters',
+    label: 'Quarter'
+  },
+  [Q_RY]: {
+    description: `4 quarters ending at the given ${ Q }`,
+    label: 'Rolling year (from quarters)'
+  },
+  [Q_YTD]: {
+    description: 'Cumulative quarters from beginning of the year',
+    label: 'Year-to-date (from quarters)'
+  },
+
+  [BM]: {
+    description: 'Bi-months',
+    label: 'Bi-month'
+  },
+  [BM_RY]: {
+    description: `6 bi-months ending at the given ${ BM }`,
+    label: 'Rolling year (from bi-months)'
+  },
+  [BM_YTD]: {
+    description: 'Cumulative bi-months from beginning of the year',
+    label: 'Year-to-date (from bi-months)'
+  },
+
+  [M]: {
+    description: 'Months',
+    label: 'Month'
+  },
+  [M_RY]: {
+    description: `12 months ending at the given ${ M }`,
+    label: 'Rolling year (from months)'
+  },
+  [M_YTD]: {
+    description: 'Cumulative months from beginning of the year',
+    label: 'Year-to-date (from months)'
+  },
+
+  [W]: {
+    description: 'Weeks',
+    label: 'Week'
+  },
+  [W_YTD]: {
+    description: 'Cumulative weeks from beginning of the year',
+    label: 'Year-to-date (from weeks)'
+  }
+};
+
+
+
+/**
+ * @function getNotation
+ * @access public
+ *
+ * @param {Date} date
+ * @param {string} type
+ *
+ * @returns {string}
+ */
+export function getNotation(date, type) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+
+  switch (type) {
+    case Y: {
+      return `${ type }_${ year }`;
+    }
+
+    case H:
+    case H_RY:
+    case H_YTD: {
+      return `${ type }_${ year }_${ Math.ceil(month / 6) }`;
+    }
+
+    case Q:
+    case Q_RY:
+    case Q_YTD: {
+      return `${ type }_${ year }_${ Math.ceil(month / 3) }`;
+    }
+
+    case BM:
+    case BM_RY:
+    case BM_YTD: {
+      return `${ type }_${ year }_${ Math.ceil(month / 2) }`;
+    }
+
+    case M:
+    case M_RY:
+    case M_YTD: {
+      return `${ type }_${ year }_${ month }`;
+    }
+
+    case W:
+    case W_YTD: {
+      // return `${ type }_${ year }_1`;
+
+      throw new Error('Weeks are not yet supported');
+    }
+
+    default: {
+      throw new Error('Invalid type');
+    }
+  }
+}
+
+
+
+/**
+ * @function getPeriod
+ * @access public
+ *
+ * @param {string} notation
+ *
+ * @returns {Period}
+ */
+export function getPeriod(notation) {
+  let [
+    type,
+    year,
+    number
+  ] = notation.split('_');
+
+  year = Number(year);
+  number = Number(number);
+
+  const result = {
+    date: {},
+    type: dictionary[type]
+  };
+
+  switch (type) {
+    case Y: {
+      result.date.from = new Date(
+        year,
+        0,
+        1
+      );
+
+      result.date.to = new Date(
+        year + 1,
+        0,
+        0
+      );
+
+      result.value = `${ year }`;
+
+      break;
+    }
+
+    case H:
+    case H_RY:
+    case H_YTD: {
+      result.date.from = new Date(
+        year,
+        6 * (number - 1),
+        1
+      );
+
+      result.date.to = new Date(
+        year,
+        6 * number,
+        0
+      );
+
+      result.value = `${ year } ${ H }${ number }`;
+
+      break;
+    }
+
+    case Q:
+    case Q_RY:
+    case Q_YTD: {
+      result.date.from = new Date(
+        year,
+        3 * (number - 1),
+        1
+      );
+
+      result.date.to = new Date(
+        year,
+        3 * number,
+        0
+      );
+
+      result.value = `${ year } ${ Q }${ number }`;
+
+      break;
+    }
+
+    case BM:
+    case BM_RY:
+    case BM_YTD: {
+      result.date.from = new Date(
+        year,
+        2 * (number - 1),
+        1
+      );
+
+      result.date.to = new Date(
+        year,
+        2 * number,
+        0
+      );
+
+      result.value = `${ year }.${ addLeadingZero(2 * number - 1) }/${ addLeadingZero(2 * number) }`;
+
+      break;
+    }
+
+    case M:
+    case M_RY:
+    case M_YTD: {
+      result.date.from = new Date(
+        year,
+        number - 1,
+        1
+      );
+
+      result.date.to = new Date(
+        year,
+        number,
+        0
+      );
+
+      result.value = `${ year }.${ addLeadingZero(number) }`;
+
+      break;
+    }
+
+    case W:
+    case W_YTD: {
+      // result.date.from = new Date(
+      //   year,
+      //   0,
+      //   1
+      // );
+
+      // result.date.to = new Date(
+      //   year,
+      //   0,
+      //   1
+      // );
+
+      // result.value = `${ year } ${ W }${ addLeadingZero(number) }`;
+
+      // break;
+
+      throw new Error('Weeks are not yet supported');
+    }
+
+    default: {
+      throw new Error('Invalid type');
+    }
+  }
+
+  switch (true) {
+    case type.includes(RY): {
+      result.date.from = new Date(
+        year - 1,
+        result.date.to.getMonth() + 1,
+        1
+      );
+
+      result.value = `${ result.value } ${ RY }`;
+
+      break;
+    }
+
+    case type.includes(YTD): {
+      result.date.from = new Date(
+        year,
+        0,
+        1
+      );
+
+      result.value = `${ result.value } ${ YTD }`;
+
+      break;
+    }
+  }
+
+  return result;
+}
+
+
+
+/**
+ * @function getPeriods
+ * @access public
+ *
+ * @param {string} notation
+ * @param {number} range
+ *
+ * @returns {Array<Period>}
+ */
+export function getPeriods(notation, range) {
+  let [
+    type,
+    year,
+    number
+  ] = notation.split('_');
+
+  year = Number(year);
+  number = Number(number);
+
+  const descending = range < 0;
+  const quantity = Math.abs(range);
+
+  const result = [];
+
+  switch (type) {
+    case Y: {
+      for (let i = 0; i < quantity; i++) {
+        result.push(
+          new Date(
+            descending ? year - i : year + i,
+            0,
+            1
+          )
+        );
+      }
+
+      break;
+    }
+
+    case H:
+    case H_RY:
+    case H_YTD: {
+      for (let i = 0; i < quantity; i++) {
+        result.push(
+          new Date(
+            year,
+            6 * ((descending ? number - i : number + i) - 1),
+            1
+          )
+        );
+      }
+
+      break;
+    }
+
+    case Q:
+    case Q_RY:
+    case Q_YTD: {
+      for (let i = 0; i < quantity; i++) {
+        result.push(
+          new Date(
+            year,
+            3 * ((descending ? number - i : number + i) - 1),
+            1
+          )
+        );
+      }
+
+      break;
+    }
+
+    case BM:
+    case BM_RY:
+    case BM_YTD: {
+      for (let i = 0; i < quantity; i++) {
+        result.push(
+          new Date(
+            year,
+            2 * ((descending ? number - i : number + i) - 1),
+            1
+          )
+        );
+      }
+
+      break;
+    }
+
+    case M:
+    case M_RY:
+    case M_YTD: {
+      for (let i = 0; i < quantity; i++) {
+        result.push(
+          new Date(
+            year,
+            (descending ? number - i : number + i) - 1,
+            1
+          )
+        );
+      }
+
+      break;
+    }
+
+    case W:
+    case W_YTD: {
+      // for (let i = 0; i < quantity; i++) {
+      //   result.push(
+      //     new Date(
+      //       year,
+      //       0,
+      //       1
+      //     )
+      //   );
+      // }
+
+      // break;
+
+      throw new Error('Weeks are not yet supported');
+    }
+
+    default: {
+      throw new Error('Invalid type');
+    }
+  }
+
+  switch (true) {
+    case descending: {
+      result.reverse();
+
+      break;
+    }
+  }
+
+  return result.map((date) => getPeriod(getNotation(date, type)));
+}
