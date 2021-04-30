7a63d757cd3ab2707a26cf8e513a753304075c2d
Adam Pacanowski <adam.pacanowski@slideworx.com>
2021-04-30 12:06:21 +0200

    getDate initialization

---
+++ src/getDate.js
@@ -0,0 +1,20 @@
+export function getDate(text, type) {
+  /*
+  Y -> 2020
+  H -> 2020 H1 // borders?
+  HRY -> 2020 H1 RY // borders?
+  HYTD -> 2020 H1 YTD // borders?
+  Q -> 2020 Q1 // borders?
+  QRY -> 2020 Q1 RY // borders?
+  QYTD -> 2020 Q1 QYTD // borders?
+  BM -> 2020.01/02 // borders?
+  BMRY -> 2020.01/02 RY // borders?
+  BMYTD -> 2020.01/02 YTD // borders?
+  M -> 2020.01 // <>.<m>; 1 <= <m> <= 12
+  MRY -> 2020.01 RY // ^
+  MYTD -> 2020.01 YTD // ^
+  W -> 2020 W01 // numberOfWeeks > week > 0
+  WYTD -> 2020 W01 ?? ^
+  */
+ return null;
+}
---
+++ src/getDate.test.js
@@ -0,0 +1,41 @@
+import { dictionary, types } from '.';
+import { getDate } from './getDate';
+
+const {
+  Y,
+
+  H,
+  HRY,
+  HYTD,
+
+  Q,
+  QRY,
+  QYTD,
+
+  BM,
+  BMRY,
+  BMYTD,
+
+  M,
+  MRY,
+  MYTD,
+
+  W,
+  WYTD,
+
+  RY = 'RY',
+  YTD = 'YTD'
+} = types;
+
+describe(dictionary[Y].label, () => {
+  test(Y, () => {
+    const text = '2020';
+
+    expect(getDate(text, Y)).toEqual({
+      date: {
+        from: new Date(2020, 0, 1),
+        to: new Date(2021, 0, 0)
+      }
+    });
+  });
+});
