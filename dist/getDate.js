317bb81b24503c5c600d8de86489b9cf8cecb994
Adam Pacanowski <adam.pacanowski@slideworx.com>
2021-05-12 11:35:41 +0200

    1.5.1

--- dist/getDate.js
+++ dist/getDate.js
@@ -57,9 +57,31 @@ function bindRegexFuncs(regexPattern, propertyNames, toNotation) {
   return functions;
 }
 
-var bmValidationFunc = function bmValidationFunc(_ref) {
-  var firstMonth = _ref.firstMonth,
-      secondMonth = _ref.secondMonth;
+var hValidationFunc = function hValidationFunc(_ref) {
+  var halfYear = _ref.halfYear;
+  var halfYearNumber = Number(halfYear);
+
+  if (halfYearNumber > 2 || halfYearNumber === 0) {
+    return false;
+  }
+
+  return true;
+};
+
+var qValidationFunc = function qValidationFunc(_ref2) {
+  var quarterYear = _ref2.quarterYear;
+  var quarterYearNumber = Number(quarterYear);
+
+  if (quarterYearNumber > 4 || quarterYearNumber === 0) {
+    return false;
+  }
+
+  return true;
+};
+
+var bmValidationFunc = function bmValidationFunc(_ref3) {
+  var firstMonth = _ref3.firstMonth,
+      secondMonth = _ref3.secondMonth;
 
   if (Number(secondMonth) - Number(firstMonth) !== 1) {
     return false;
@@ -76,8 +98,8 @@ var bmValidationFunc = function bmValidationFunc(_ref) {
   return true;
 };
 
-var mValidationFunc = function mValidationFunc(_ref2) {
-  var month = _ref2.month;
+var mValidationFunc = function mValidationFunc(_ref4) {
+  var month = _ref4.month;
 
   if (Number(month) > 12) {
     return false;
@@ -91,94 +113,96 @@ function getDate(text, type) {
   text = text.toUpperCase().replace(/\s\s+/g, ' ').trim();
 
   if (type === Y) {
-    regexFuncs = bindRegexFuncs(/^(\d{4})$/i, ['year'], function (_ref3) {
-      var year = _ref3.year;
+    regexFuncs = bindRegexFuncs(/^(\d{4})$/i, ['year'], function (_ref5) {
+      var year = _ref5.year;
       return "".concat(Y, "_").concat(year);
     });
   } else if (type === H) {
-    regexFuncs = bindRegexFuncs("^(\\d{4}) ".concat(H, "(\\d{1})$"), ['year', 'halfYear'], function (_ref4) {
-      var year = _ref4.year,
-          halfYear = _ref4.halfYear;
-      return "".concat(H, "_").concat(year, "_").concat(halfYear);
-    });
-  } else if (type === HRY) {
-    regexFuncs = bindRegexFuncs("^(\\d{4}) ".concat(H, "(\\d{1}) RY$"), ['year', 'halfYear'], function (_ref5) {
-      var year = _ref5.year,
-          halfYear = _ref5.halfYear;
-      return "".concat(HRY, "_").concat(year, "_").concat(halfYear);
-    });
-  } else if (type === HYTD) {
-    regexFuncs = bindRegexFuncs("^(\\d{4}) ".concat(H, "(\\d{1}) YTD$"), ['year', 'halfYear'], function (_ref6) {
+    regexFuncs = bindRegexFuncs("^(\\d{4}) ".concat(H, "(\\d{1})$"), ['year', 'halfYear'], function (_ref6) {
       var year = _ref6.year,
           halfYear = _ref6.halfYear;
-      return "".concat(HYTD, "_").concat(year, "_").concat(halfYear);
-    });
-  } else if (type === Q) {
-    regexFuncs = bindRegexFuncs("^(\\d{4}) ".concat(Q, "(\\d{1})$"), ['year', 'quarterYear'], function (_ref7) {
+      return "".concat(H, "_").concat(year, "_").concat(halfYear);
+    }, hValidationFunc);
+  } else if (type === HRY) {
+    regexFuncs = bindRegexFuncs("^(\\d{4}) ".concat(H, "(\\d{1}) RY$"), ['year', 'halfYear'], function (_ref7) {
       var year = _ref7.year,
-          quarterYear = _ref7.quarterYear;
-      return "".concat(Q, "_").concat(year, "_").concat(quarterYear);
-    });
-  } else if (type === QRY) {
-    regexFuncs = bindRegexFuncs("^(\\d{4}) ".concat(Q, "(\\d{1}) RY$"), ['year', 'quarterYear'], function (_ref8) {
+          halfYear = _ref7.halfYear;
+      return "".concat(HRY, "_").concat(year, "_").concat(halfYear);
+    }, hValidationFunc);
+  } else if (type === HYTD) {
+    regexFuncs = bindRegexFuncs("^(\\d{4}) ".concat(H, "(\\d{1}) YTD$"), ['year', 'halfYear'], function (_ref8) {
       var year = _ref8.year,
-          quarterYear = _ref8.quarterYear;
-      return "".concat(QRY, "_").concat(year, "_").concat(quarterYear);
-    });
-  } else if (type === QYTD) {
-    regexFuncs = bindRegexFuncs("^(\\d{4}) ".concat(Q, "(\\d{1}) YTD$"), ['year', 'quarterYear'], function (_ref9) {
+          halfYear = _ref8.halfYear;
+      return "".concat(HYTD, "_").concat(year, "_").concat(halfYear);
+    }, hValidationFunc);
+  } else if (type === Q) {
+    regexFuncs = bindRegexFuncs("^(\\d{4}) ".concat(Q, "(\\d{1})$"), ['year', 'quarterYear'], function (_ref9) {
       var year = _ref9.year,
           quarterYear = _ref9.quarterYear;
-      return "".concat(QYTD, "_").concat(year, "_").concat(quarterYear);
-    });
-  } else if (type === BM) {
-    regexFuncs = bindRegexFuncs(/^(\d{4})\.(\d{2})\/(\d{2})$/i, ['year', 'firstMonth', 'secondMonth'], function (_ref10) {
+      return "".concat(Q, "_").concat(year, "_").concat(quarterYear);
+    }, qValidationFunc);
+  } else if (type === QRY) {
+    regexFuncs = bindRegexFuncs("^(\\d{4}) ".concat(Q, "(\\d{1}) RY$"), ['year', 'quarterYear'], function (_ref10) {
       var year = _ref10.year,
-          firstMonth = _ref10.firstMonth;
+          quarterYear = _ref10.quarterYear;
+      return "".concat(QRY, "_").concat(year, "_").concat(quarterYear);
+    }, qValidationFunc);
+  } else if (type === QYTD) {
+    regexFuncs = bindRegexFuncs("^(\\d{4}) ".concat(Q, "(\\d{1}) YTD$"), ['year', 'quarterYear'], function (_ref11) {
+      var year = _ref11.year,
+          quarterYear = _ref11.quarterYear;
+      return "".concat(QYTD, "_").concat(year, "_").concat(quarterYear);
+    }, qValidationFunc);
+  } else if (type === BM) {
+    regexFuncs = bindRegexFuncs(/^(\d{4})\.(\d{2})\/(\d{2})$/i, ['year', 'firstMonth', 'secondMonth'], function (_ref12) {
+      var year = _ref12.year,
+          firstMonth = _ref12.firstMonth;
       return "".concat(BM, "_").concat(year, "_").concat(Math.floor(Number(firstMonth) / 2) + 1);
     }, bmValidationFunc);
   } else if (type === BMRY) {
-    regexFuncs = bindRegexFuncs(/^(\d{4})\.(\d{2})\/(\d{2}) RY$/i, ['year', 'firstMonth', 'secondMonth'], function (_ref11) {
-      var year = _ref11.year,
-          firstMonth = _ref11.firstMonth;
+    regexFuncs = bindRegexFuncs(/^(\d{4})\.(\d{2})\/(\d{2}) RY$/i, ['year', 'firstMonth', 'secondMonth'], function (_ref13) {
+      var year = _ref13.year,
+          firstMonth = _ref13.firstMonth;
       return "".concat(BMRY, "_").concat(year, "_").concat(Math.floor(Number(firstMonth) / 2) + 1);
     }, bmValidationFunc);
   } else if (type === BMYTD) {
-    regexFuncs = bindRegexFuncs(/^(\d{4})\.(\d{2})\/(\d{2}) YTD$/i, ['year', 'firstMonth', 'secondMonth'], function (_ref12) {
-      var year = _ref12.year,
-          firstMonth = _ref12.firstMonth;
+    regexFuncs = bindRegexFuncs(/^(\d{4})\.(\d{2})\/(\d{2}) YTD$/i, ['year', 'firstMonth', 'secondMonth'], function (_ref14) {
+      var year = _ref14.year,
+          firstMonth = _ref14.firstMonth;
       return "".concat(BMYTD, "_").concat(year, "_").concat(Math.floor(Number(firstMonth) / 2) + 1);
     }, bmValidationFunc);
   } else if (type === M) {
-    regexFuncs = bindRegexFuncs(/^(\d{4})\.(\d{2})$/i, ['year', 'month'], function (_ref13) {
-      var year = _ref13.year,
-          month = _ref13.month;
+    regexFuncs = bindRegexFuncs(/^(\d{4})\.(\d{2})$/i, ['year', 'month'], function (_ref15) {
+      var year = _ref15.year,
+          month = _ref15.month;
       return "".concat(M, "_").concat(year, "_").concat(month);
     }, mValidationFunc);
   } else if (type === MRY) {
-    regexFuncs = bindRegexFuncs(/^(\d{4})\.(\d{2}) RY$/i, ['year', 'month'], function (_ref14) {
-      var year = _ref14.year,
-          month = _ref14.month;
+    regexFuncs = bindRegexFuncs(/^(\d{4})\.(\d{2}) RY$/i, ['year', 'month'], function (_ref16) {
+      var year = _ref16.year,
+          month = _ref16.month;
       return "".concat(MRY, "_").concat(year, "_").concat(month);
     }, mValidationFunc);
   } else if (type === MYTD) {
-    regexFuncs = bindRegexFuncs(/^(\d{4})\.(\d{2}) YTD$/i, ['year', 'month'], function (_ref15) {
-      var year = _ref15.year,
-          month = _ref15.month;
+    regexFuncs = bindRegexFuncs(/^(\d{4})\.(\d{2}) YTD$/i, ['year', 'month'], function (_ref17) {
+      var year = _ref17.year,
+          month = _ref17.month;
       return "".concat(MYTD, "_").concat(year, "_").concat(month);
     }, mValidationFunc);
   } else if (type === W) {
-    regexFuncs = bindRegexFuncs("^(\\d{4}) ".concat(W, "(\\d{2})$"), ['year', 'week'], function (_ref16) {
-      var year = _ref16.year,
-          week = _ref16.week;
+    regexFuncs = bindRegexFuncs("^(\\d{4}) ".concat(W, "(\\d{2})$"), ['year', 'week'], function (_ref18) {
+      var year = _ref18.year,
+          week = _ref18.week;
       return "".concat(W, "_").concat(year, "_").concat(week);
     });
   } else if (type === WYTD) {
-    regexFuncs = bindRegexFuncs("^(\\d{4}) ".concat(W, "(\\d{2}) YTD$"), ['year', 'week'], function (_ref17) {
-      var year = _ref17.year,
-          week = _ref17.week;
+    regexFuncs = bindRegexFuncs("^(\\d{4}) ".concat(W, "(\\d{2}) YTD$"), ['year', 'week'], function (_ref19) {
+      var year = _ref19.year,
+          week = _ref19.week;
       return "".concat(WYTD, "_").concat(year, "_").concat(week);
     });
+  } else {
+    return null;
   }
 
   if (!regexFuncs.validate(text)) {
